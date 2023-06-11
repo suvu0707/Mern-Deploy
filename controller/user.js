ab0e0c60b3fd3548c6c6543ae4d28bc1c9fee49a
Suvendu <suvu0707@gmail.com>
2023-06-11 17:02:13 +0530

    hgghh

--- controller/user.js
+++ controller/user.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 // const index = fs.readFileSync('index.html', 'utf-8');
-const data = JSON.parse(fs.readFileSync(path.resolve(__dirname,'data.json'), 'utf-8'));
+const data = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../data.json'), 'utf-8'));
 const users = data.users;
 
 exports.createUser = (req, res) => {
